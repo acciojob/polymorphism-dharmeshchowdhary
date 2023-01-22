e57a1eb99df777fb542101dfba305e0d9338f74b
dharmesh_chowdhary <dharmeshchowdhary2000@gmail.com>
2023-01-22 18:52:52 +0530

    Code

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,34 @@
 package com.driver;
 
-public class Main {
+import java.util.*;
 
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Product p = new Product();
+        int x = sc.nextInt(), y = sc.nextInt();
+        System.out.println(p.product(x , y));
+
+        int z = sc.nextInt();
+        System.out.println(p.product(x , y, z));
+
+        double a = sc.nextDouble(), b = sc.nextDouble();
+        System.out.println(p.product(a , b));
+
+        sc.close();
+    }
+
+    public static class Product{
+        public int product(int x, int y) {
+            return x * y;
+        }
+
+        public int product(int x, int y, int z) {
+            return x * y * z;
+        }
+
+        public double product(double x, double y) {
+            return x * y;
+        }
+    }
 }
